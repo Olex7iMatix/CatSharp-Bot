d6cdb4169b0cdd6c35966b46eb0843b60fb625dc
Olex7iMatix <86416834+Olex7iMatix@users.noreply.github.com>
2022-11-20 14:37:54 +0100

    mongoose added

---
+++ dara/models/ticket.model.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require("mongoose");
+
+const shema = Schema({
+    ticketOwnerId: { type: String, unique: true },
+    ticketType: { type: String, default: "Bug Report" },
+    deleted: { type: Boolean, default: false },
+    messages: { type: [Object], default: [] }
+})
+
+module.exports = model("tickets", schema)
--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,18 @@
 require("dotenv").config()
-const { Client, GatewayIntentBits, Partials } = require( "discord.js" )
+const { Client, GatewayIntentBits, Partials } = require( "discord.js" );
+const mongoose = require("mongoose");
 const bot = new Client({ intents: [GatewayIntentBits.Guilds], partials: [Partials.Channel] });
 
 const slashCommands = require(__dirname + '/handlers/slashCommands.handler')
 
+const {TOKEN, MOGNOOSE_USERNAME, MONGOOSE_USER_PASS} = process.env
+
 slashCommands(bot)
 bot.on("ready", () => {
     console.log("Ready!");   
+
+    mongoose.connect(`mongodb+srv://${MOGNOOSE_USERNAME}:${MONGOOSE_USER_PASS}@botcluster.rqrp4ww.mongodb.net/?retryWrites=true&w=majority`, {  })
+        .then(() => console.log("Connected to db!"))
 })
 
-bot.login(process.env.TOKEN)
+bot.login(TOKEN)
